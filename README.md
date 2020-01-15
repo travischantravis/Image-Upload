437ed886997240cec0ff20e94a1095f21e1a223e
travischantravis <chancheukyin4463@gmail.com>
2020-01-14 23:37:57 -0800

    handled post request

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # Image Upload
 
 Express.js
-put `css` and `js` in public folder
+
+- put `css` and `js` in public folder
+
+Dependencies
+
+- Express
+- Multer: image upload
+- ejs: template engine
+- Materialize: Require jQuery!
+
+Folders:
+
+- public
+  - uploads
+- views
+  - index.ejs
+
+Form, file input uses Materialize
--- server.js
+++ server.js
@@ -1,12 +1,55 @@
 const express = require("express");
-const app = express();
+const multer = require("multer");
+const ejs = require("ejs");
 const path = require("path");
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+const app = express();
+const port = 3000;
+// const upload = multer({ dest: __dirname + "/uploads/images" });
+
+// Set storage engine
+const storage = multer.diskStorage({
+  destination: "./public/uploads/",
+  filename: function(req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  }
 });
 
-app.use("/", express.static(path.join(__dirname, "public")));
+// Init Upload
+const upload = multer({
+  storage: storage
+}).single("myImage");
+
+// EJS
+app.set("view engine", "ejs");
+
+// Public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/upload", (req, res) => {
+  upload(req, res, err => {
+    if (err) {
+      res.render("index", {
+        msg: err
+      });
+    } else {
+      console.log(req.file);
+      res.send("test");
+    }
+  });
+});
+
+// app.post("/upload", upload.single("photo"), (req, res) => {
+//   if (req.file) {
+//     res.json(req.file);
+//   } else throw "error";
+// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
